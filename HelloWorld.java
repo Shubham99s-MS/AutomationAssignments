bf0753b2ebdba79a88077935a6c6afeb8fa550d8
Shubham99s-MS <shubhamin99qa@gmail.com>
2024-12-27 21:38:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package com.cretio.crm.language.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Print text with new line
+		System.out.println("Hello World");
+		
+		// Print text with new line
+		System.out.print("Hello");
+		System.out.print(" world " );
+		
+		// Print using formatting
+		System.out.printf("Hello,This is %s and this is written %d times","Shubham",5);
+		
+		
+
+	}
+
+}
